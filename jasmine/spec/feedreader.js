5bacb27e9f0ee6dd3787da287d12692da972f7d8
Karren Zoleta <zoletakarren@gmail.com>
2018-08-05 21:18:13 -0700

    Initial Entries and New Feed Selection
    1. ensures when the loadFeed function is called and completes its work, there is at least a single .entry element within the .feed container.
    2.  ensures when a new feed is loaded by the loadFeed function that the content actually changes.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -64,7 +64,6 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-
     describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -101,6 +100,7 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -109,10 +109,38 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
+         /* Kaye: Call the loadfeed function and after it completes it's work
+          * as required, I use beforeEach() and done()
+          */
+
+         beforeEach(function(done) {
+            loadFeed(0, done);
+        });    
+
+          it('completes work', function() {
+            expect($('.entry').length).not.toBe(0);
+        });
+    });
+
+
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
+
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+
+          beforeEach(function(done) {
+            loadFeed(0);
+            prevTopPost = $('entry').eq(0).html();
+
+            loadFeed(1, done);
+        });
+
+          it('content changes', function() {
+            expect($('.entry').eq(0).html()).not.toEqual(prevTopPost);
+          });
+     });
 }());
